50378e17b182ad0215916b84fed0909bc69dd77b
Hsinhan Lin <13601628+hsinhandev@users.noreply.github.com>
2022-01-31 10:12:01 +0100

    Change website & get property link

--- utils/crawler.py
+++ utils/crawler.py
@@ -1,11 +1,19 @@
 from typing import List, Dict
-import re
-import selenium
 from bs4 import BeautifulSoup
 import pandas as pd
+
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+
+# TODO use closure to create browser
+# =================================================================init
+service = Service("./vendor/geckodriver")
+firefox_options = Options()
+# TODO debug
+# firefox_options.add_argument("--headless")  # Ensure GUI is off
+browser = webdriver.Firefox(service=service, options=firefox_options)
 
 
 def extract_property_link(num_pages: int) -> List[str]:
@@ -20,50 +28,44 @@ def extract_property_link(num_pages: int) -> List[str]:
 
     print(f"====== Collection pages {num_pages}")
 
-    # elissa_url = "https://immoelissa.be/immobilier/?sort=prix-c"
-    elissa_url_pages = f"https://immoelissa.be/immobilier/?pg={num_pages}&sort=prix-c"
+    immoweb_url_page = f"https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&page={num_pages}&orderBy=relevance"
+    browser.get(immoweb_url_page)
 
-    # =================================================================init
-    service = Service("./vendor/geckodriver")
-    firefox_options = Options()
-    # TODO debug
-    firefox_options.add_argument("--headless")  # Ensure GUI is off
-    browser = webdriver.Firefox(service=service, options=firefox_options)
+    # Not working here??
+    browser.implicitly_wait(10)
 
-    browser.get(elissa_url_pages)
-
-    # browser.implicitly_wait(1)
-
-    html = browser.page_source
-    soup = BeautifulSoup(html, "lxml")
-
-    browser.close()
-
-    links = []
-    for card in soup.select("div.results-view .lf-item>a"):
-        property_link = card.get("href")
-        links.append(property_link)
+    property_list = browser.find_elements(By.CSS_SELECTOR, "ul#main-content article a")
+    property_link = []
+    for link in property_list:
+        property_link.append(link.get_attribute("href"))
 
     print(f"====== Collection pages {num_pages} Finished =======")
-    return links
+    return property_link
 
 
-def get_property_link_until_thousnad() -> Dict[str, str]:
-    """[This function should call extract_property_link 1000 times, to get all the property link]
+def get_property_link_until_ten_thousnad() -> Dict[str, str]:
+    """[This function should call extract_property_link until 10,000 property link]
 
     Returns:
         [type]: [description]
     """
     properties_link = {"link": []}
 
-    for num in range(1, 5):
+    # TODO fix crash at page 12
+    for num in range(1, 100):
         for href in extract_property_link(num):
             properties_link["link"].append(href)
-
     return properties_link
 
 
 if __name__ == "__main__":
-    property_link_output = get_property_link_until_thousnad()
-    df = pd.DataFrame(property_link_output)
-    df.to_csv(r"./data/property_link.csv", index=False, header=True)
+    try:
+        property_link_output = get_property_link_until_ten_thousnad()
+        df = pd.DataFrame(property_link_output)
+        df.to_csv(r"./data/property_link.csv", index=False, header=True)
+    except NoSuchElementException:
+        time.sleep(2)
+    except TimeoutException as ex:
+        print(ex.message)
+    finally:
+        browser.quit()
